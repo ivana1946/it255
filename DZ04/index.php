ce74f13184acedf02738339c74d0d098db7f92da
ivana1946 <ivanast14071994@gmail.com>
2018-11-23 21:38:34 +0100

    Cetvrti domaci

---
+++ DZ04/index.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Početna strana</title>
+<link rel="stylesheet" href="stil.css">
+</head>
+<body>
+<table align="center" width="900" border="1">
+<tr>
+<td colspan="2" align="center">
+ <img class="img-responsive img-portfolio img-hover" src="slike/volonteri.jpg" alt="">
+<ul class="meni"> 
+<li><a href="index.php">Početna</a></li>
+<li><a href="registracija.php">Registracija</a></li>
+<li><a href="volonteri.php">Lista volontera</a></li>
+</ul>
+</td>
+</tr>
+<tr>
+<td width="200" valign="top">
+
+</td>
+<td valign="top">
+<table align="center" border="1" cellpadding="3">
+<tr>
+<td colspan="4" align="center">
+<span class="naslov">Lista volonterskih akcija</span>
+</td>
+</tr>
+<tr>
+<div class="panel-body">
+                        <p>Pomozite svojom humanošću drugim i uživajte u raznim akcijama. </p>
+                        <a href="#" class="btn btn-default">Opširnije</a>
+<td style ="color:red">Naziv</td>
+<td style ="color:red">Lokacija</td>
+<td style ="color:red">Datum početka</td>
+<td style ="color:red">Datum kraja</td>
+</tr>
+
+
+
+</table>
+</td>
+</tr>
+<tr>
+<td colspan="2" align="center" style ="color:red">Ivana Stanković, 1946. </td>
+</tr>
+</table>
+</body>
+</html>
